8430743456f225cc41d7d83408754c5740facd92
AlexP <68079031+xelaz98@users.noreply.github.com>
2025-01-02 20:09:42 +0200

    Update ListGroup.tsx

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -4,6 +4,7 @@ function ListGroup() {
   return (
     <>
       <h1>List Group</h1>
+      {items.length === 0 ? <p>No items found</p> : null}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
