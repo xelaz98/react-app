8320382be3e9ad57ee5775a352de0f384e47ff1a
AlexP <68079031+xelaz98@users.noreply.github.com>
2025-01-02 20:06:43 +0200

    Initial commit

---
+++ src/components/ListGroup.tsx
@@ -0,0 +1,16 @@
+function ListGroup() {
+  const items = ["New York", "Tokyo", "San Francisco", "London", "Paris"];
+
+  return (
+    <>
+      <h1>List Group</h1>
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default ListGroup;
