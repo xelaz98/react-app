4c6df6e6ae9ff4e94befd4fbf2e997c271da4643
AlexP <68079031+xelaz98@users.noreply.github.com>
2025-01-02 20:14:01 +0200

    Update ListGroup.tsx

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,10 +1,13 @@
 function ListGroup() {
   const items = ["New York", "Tokyo", "San Francisco", "London", "Paris"];
+  const getMessage = () => {
+    return items.length === 0 ? <p>No items found</p> : null;
+  };
 
   return (
     <>
       <h1>List Group</h1>
-      {items.length === 0 ? <p>No items found</p> : null}
+      {getMessage()}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
